f2037ae079ac8c6a23e95eae2ac0f8afb1c4cb81
Walter <walterz@student.unimelb.edu.au>
2017-08-28 18:15:54 +1000

    update

---
+++ Beam/database/database.sql
@@ -0,0 +1,5 @@
+CREATE TABLE user_table (
+  user_id INT(10) UNSIGNED NOT NULL,
+  pword INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (user_id)
+) ENGINE=INNODB;
---
+++ Beam/database/test.py
@@ -0,0 +1,32 @@
+from datetime import datetime  
+from flask import render_template  
+import MySQLdb as mdb
+  
+import pypyodbc  
+  
+from datetime  import datetime  
+  
+from flask  import render_template, redirect, request  
+ 
+ 
+ 
+# creating connection Object which will contain SQL Server Connection  
+connection = pypyodbc.connect('Driver={SQL Server};Server=.;Database=tscelsi;uid=tscelsi;pwd=tscelsi_2017')# Creating Cursor  
+  
+cursor = connection.cursor()  
+cursor.execute("SELECT * FROM Customer")  
+s = "<table style='border:1px solid red'>"  
+for row in cursor:  
+    s = s + "<tr>"  
+for x in row:  
+    s = s + "<td>" + str(x) + "</td>"  
+s = s + "</tr>"  
+  
+  
+connection.close()  
+  
+@app.route('/')  
+@app.route('/home')  
+def home():  
+  
+    return "<html><body>" + s + "</body></html>"

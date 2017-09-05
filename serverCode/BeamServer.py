a9ef6142e1e28e4c37c38900b75e83fe8f4c7840
tscelsi <tscelsi@student.unimelb.edu.au>
2017-09-05 22:27:22 +1000

    added virtualenv functionality
    virtualenv allows for everyone to pull a workable version of the BeamServer code. No need to install packages etc. they are all already there. To run: need to run the 'activate' file in beamServer\Scripts\activate

--- serverCode/BeamServer.py
+++ serverCode/BeamServer.py
@@ -2,40 +2,40 @@ import socket #for sockets
 import sys # for exit
 import mysql.connector
 import json
-from thread import *
+from threading import *
 
 HOST = '' #means all available interfaces
 PORT = 8889 #non-privileged port
 
 try:
-    #create an AF_INET, TCP socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print "Socket created"
-except socket.error, msg:
-        print 'Failed to create socket. Error code: ' + str(msg[0]) + ', Error Message: ' + msg[1]
-        sys.exit();
+	#create an AF_INET, TCP socket
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	print ("Socket created")
+except socket.error:
+		print ('Failed to create socket.')
+		sys.exit();
 
 try:
-    s.bind((HOST, PORT))
-    print "socket bound on: " + HOST + "," + str(PORT)
-except socket.error, msg:
-    print 'Bind failed. Error Code: ' + str(msg[0]) + ', Message: ' + msg[1]
-    sys.exit()
+	s.bind((HOST, PORT))
+	print ("socket bound on: " + HOST + "," + str(PORT))
+except socket.error:
+	print ('Bind failed.')
+	sys.exit()
 
 s.listen(2)
-print "listening..."
+print ("listening...")
 
 
 #thread to handle client connections:
 def clientthread(conn):
-    conn.send("Welcome to the server. Type something and hit enter.\n")
-    cnx = mysql.connector.connect(user= 'tscelsi', password= 'tscelsi_2017',host='info20003db.eng.unimelb.edu.au',database='tscelsi')
-    cursor = cnx.cursor()
-    query = ("SELECT * from users")
-    cursor.execute(query)
-    data = conn.recv(1024)
-    print data.strip() + ": this is the data"
-    if (data.strip() == "sendMeData"):
+	conn.send("Welcome to the server. Type something and hit enter.\n")
+	cnx = mysql.connector.connect(user= 'tscelsi', password= 'tscelsi_2017',host='info20003db.eng.unimelb.edu.au',database='tscelsi')
+	cursor = cnx.cursor()
+	query = ("SELECT * from users")
+	cursor.execute(query)
+	data = conn.recv(1024)
+	print (data.strip() + ": this is the data")
+	if (data.strip() == "sendMeData"):
 		
 		
 		
@@ -47,17 +47,17 @@ def clientthread(conn):
 		json_data = json.dumps(data)
 		conn.send(json_data)
 		
-    conn.sendall("bye")
-    cursor.close()
-    cnx.close()
-    #came out of loop, close connection
-    conn.close()
+	conn.sendall("bye")
+	cursor.close()
+	cnx.close()
+	#came out of loop, close connection
+	conn.close()
 
 while 1:
-    conn, addr = s.accept()
-    #display client info
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
-    start_new_thread(clientthread, (conn,))
+	conn, addr = s.accept()
+	#display client info
+	print ('Connected with ' + addr[0] + ':' + str(addr[1]))
+	start_new_thread(clientthread, (conn,))
 
 #close socket    
 s.close()

b654539b79fae34e57621c3847104fc376d5c9de
haziq <msamuri@student.unimelb.edu.au>
2017-09-07 14:59:49 +1000

    testing message.java class

---
+++ Beam/app/src/main/java/com/example/grace/myapplication/Message.java
@@ -0,0 +1,14 @@
+package main.java.com.example.grace.myapplication;
+
+/**
+ * Created by haziq on 7/09/17.
+ * this is a template for the message class to be used in our in-app chat
+ */
+
+public class Message {
+    public static void SendMessage(String message, int userID){
+
+    }
+}
+
+

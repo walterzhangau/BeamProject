e8c1ddcd5daa5fa5f62bbea9b07645cfd84d5c46
haziq <msamuri@student.unimelb.edu.au>
2017-09-09 20:15:58 +1000

    pushing new message class and java to json converter

--- Beam/app/src/main/java/com/example/grace/myapplication/Message.java
+++ Beam/app/src/main/java/com/example/grace/myapplication/Message.java
@@ -1,6 +1,6 @@
-package main.java.com.example.grace.myapplication;
-import java.sql.Timestamp;
-import java.util.Date;
+package com.example.grace.myapplication;
+
+
 /**
  * Created by haziq on 7/09/17.
  * this is a template for the message class to be used in our in-app chat
@@ -10,22 +10,45 @@ public class Message {
 
     private int senderID;
     private int receiverID;
-    private String message;
-    private Timestamp timestamp;
+    private String messageBody;
 
 
-    public Message(int senderID, int receiverID, String message, Timestamp timestamp){
+    public Message(int senderID, int receiverID, String messageBody){
         this.senderID = senderID;
         this.receiverID = receiverID;
-        this.message = message;
-        this.timestamp = timestamp;
+        this.messageBody = messageBody;
     }
 
-    public static void SendMessage(String message, int userID){
+    public static void SendMessage(String messageBody, int userID){
         //SEND THIS MESSAGING SHIT TO
         //THIS USER ID
 
     }
+
+    public int getSenderID() {
+        return senderID;
+    }
+
+    public void setSenderID(int senderID) {
+        this.senderID = senderID;
+    }
+
+    public int getReceiverID() {
+        return receiverID;
+    }
+
+    public void setReceiverID(int receiverID) {
+        this.receiverID = receiverID;
+    }
+
+    public String getMessageBody() {
+        return messageBody;
+    }
+
+    public void setMessageBody(String messageBody) {
+        this.messageBody = messageBody;
+    }
+
 }
 
 

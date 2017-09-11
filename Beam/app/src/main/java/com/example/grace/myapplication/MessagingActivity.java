40572461bd2b1d041891eaf19a982186a3ccba4d
Evan Lewis <evanl2@student.unimelb.edu.au>
2017-09-11 16:39:22 +1000

    Added back button to messaging activity

--- Beam/app/src/main/java/com/example/grace/myapplication/MessagingActivity.java
+++ Beam/app/src/main/java/com/example/grace/myapplication/MessagingActivity.java
@@ -1,10 +1,13 @@
 package com.example.grace.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MessagingActivity extends AppCompatActivity {
@@ -22,8 +25,36 @@ public class MessagingActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+
             }
+
+
         });
+        setupActionBar();
+    }
+
+
+
+
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the back button in the action bar.
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent intent = new Intent(MessagingActivity.this, NavigationBarActivity.class);
+        startActivity(intent);
+        finish();
+        return true;
+
+    }
 
 
 
@@ -32,8 +63,3 @@ public class MessagingActivity extends AppCompatActivity {
 
 
     }
-
-
-
-
-}

974fd7af3b04ba874f0bfe6436fc6e322c08e94b
gracegeddes <ggeddes@student.unimelb,edu.au>
2017-09-20 15:38:38 +1000

    Slight Changes to the MessageAPI and print statements in the MessagingActivity

--- Beam/app/src/main/java/com/example/grace/messaging/SocketConnection.java
+++ Beam/app/src/main/java/com/example/grace/messaging/SocketConnection.java
@@ -6,18 +6,17 @@ import com.github.nkzawa.socketio.client.Socket;
  * Created by haziq on 9/09/17.
  * Edited by Grace on 16/09/17
  */
-public class SocketConnection {
-    public Socket bSocket;
-    SocketConnection(Socket bSocket){
-        this.bSocket = bSocket;
-    }
-
-    {
-//      Open the socket
-        try {
-            bSocket = IO.socket("http://127.0.0.1:5000");
-        }
-        catch (java.net.URISyntaxException e)
-        {}
-    }
-}
+//public class SocketConnection {
+//    public Socket bSocket;
+//    SocketConnection(Socket bSocket){
+//        this.bSocket = bSocket;
+//    }
+////      Open the socket
+//        try {
+//            bSocket = IO.socket("http://127.0.0.1:5000");
+//        }
+//        catch (java.net.URISyntaxException e)
+//        {
+//        }
+//
+//}

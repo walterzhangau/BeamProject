bab725c0bddd590ea3b4d1da67e184ca9ce2f709
Evan Lewis <evanl2@student.unimelb.edu.au>
2017-10-10 19:27:00 +1100

    Account creation now tells user if account was successfully created Neatened up the layout of account creation

--- Beam/app/src/main/java/com/example/grace/myapplication/AccountCreation.java
+++ Beam/app/src/main/java/com/example/grace/myapplication/AccountCreation.java
@@ -1,9 +1,15 @@
 package com.example.grace.myapplication;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -17,12 +23,37 @@ import java.util.ArrayList;
 
 public class AccountCreation extends AppCompatActivity {
 
+    final String USERCREATED = "User creation success";
+
+    TextView email_input;
+    AccountCreationTask ACtask;
+
+    //Main view
+    View ACform;
+
+    // View for loading screen
+    View ACprogress;
+
+
+    //Tells user account was created
+    View AccountCreatedDisplay;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setupActionBar();
         setContentView(R.layout.activity_account_creation);
+        setViews();
         SetUpCreateAccountButton();
+
+    }
+
+    private void setViews(){
+        ACform = findViewById(R.id.AccountCreation_form);
+        ACprogress = findViewById(R.id.AccountCreation_progress);
 
     }
 
@@ -53,10 +84,12 @@ public class AccountCreation extends AppCompatActivity {
 
         final Button CreateAccountButton   = (Button)   findViewById(R.id.accountCreation_Button);
 
-        final TextView email_input = (TextView) findViewById(R.id.accountCreation_Email);
+        email_input = (TextView) findViewById(R.id.accountCreation_Email);
         final TextView username_input = (TextView) findViewById(R.id.accountCreation_Username);
         final TextView password1_input = (TextView) findViewById(R.id.accountCreation_Password1);
         TextView password2_input = (TextView) findViewById(R.id.accountCreation_Password2);
+
+        AccountCreatedDisplay = findViewById(R.id.AccountCreation_display);
 
         CreateAccountButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -81,8 +114,6 @@ public class AccountCreation extends AppCompatActivity {
 
 
 
-                //Write code here (Use "email_input.gettext")
-
             }
         });
     }
@@ -93,8 +124,106 @@ public class AccountCreation extends AppCompatActivity {
 
 
         ServerConnection serverConnection = new ServerConnection();
-        serverConnection.makeServerRequest("CreateUser", KeyTags, Keys, 3, this, true);
+        serverConnection.makeServerRequest("CreateUser", KeyTags, Keys, 3, this, false);
+        ACtask = new AccountCreationTask();
+        ACtask.execute();
 
     }
 
+
+
+
+    public class AccountCreationTask extends AsyncTask<Void, Void, Boolean> {
+
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            try {
+                while(JSONResponse.response == null) {
+                    Thread.sleep(20);
+                    Log.e("Wating", "waiting");
+                }
+
+                if (JSONResponse.response.getString("Message").equals(USERCREATED)){
+                    return true;
+
+                }
+                else{
+                    return false;
+                }
+
+
+
+            }catch(Exception e){e.printStackTrace();}
+
+            return false;
+        }
+
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+
+            if(success){
+                AccountCreatedDisplay.setVisibility(View.VISIBLE);
+
+            }
+            else{
+                email_input.setText("USERNAME TAKEN");
+
+            }
+
+            ACtask = null;
+            showProgress(false);
+            JSONResponse.response = null;
+        }
+
+        @Override
+        protected void onCancelled() {
+            ACtask = null;
+            showProgress(false);
+        }
+    }
+
+
+    /**
+     * Shows the progress UI and hides the friendslist form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            ACform.setVisibility(show ? View.GONE : View.VISIBLE);
+            ACform.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    ACform.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            ACprogress.setVisibility(show ? View.VISIBLE : View.GONE);
+            ACprogress.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    ACprogress.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+
+
+
+
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            ACprogress.setVisibility(show ? View.VISIBLE : View.GONE);
+            ACform.setVisibility(show ? View.GONE : View.VISIBLE);
+
+        }
+    }
 }

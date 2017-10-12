91fa0b9dc296b15e0ff20a09d7b3bc820c014def
Evan Lewis <evanl2@student.unimelb.edu.au>
2017-10-12 18:22:40 +1100

    Opening messaging via navigation bar changes button from "send to " to "send to chat room"

--- Beam/app/src/main/java/com/example/grace/myapplication/NavigationBarActivity.java
+++ Beam/app/src/main/java/com/example/grace/myapplication/NavigationBarActivity.java
@@ -69,6 +69,7 @@ public class NavigationBarActivity extends AppCompatActivity
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(NavigationBarActivity.this, MessagingActivity.class);
+                intent.putExtra("FIRST_NAME", "Chat Room");
                 startActivity(intent);
                 finish();
 

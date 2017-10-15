7e17aff4fffdb736e0744900f946ba15cf675c1e
Evan Lewis <evanl2@student.unimelb.edu.au>
2017-10-15 13:50:38 +1100

    Added intent to friend beam button
    Fixed gitignore

--- Beam/app/src/main/java/com/example/grace/myapplication/FriendsActivity.java
+++ Beam/app/src/main/java/com/example/grace/myapplication/FriendsActivity.java
@@ -289,7 +289,7 @@ public class FriendsActivity extends AppCompatActivity {
     }
 
 
-    private Button beamButton(String friend_username,int Status){
+    private Button beamButton(final String friend_username, int Status){
 
 
         Button button = new Button(this);
@@ -299,6 +299,10 @@ public class FriendsActivity extends AppCompatActivity {
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+                    Intent intent = new Intent(FriendsActivity.this, SampleCamActivity.class);
+                    intent.putExtra("user", friend_username);
+                    startActivity(intent);
+                    finish();
                 }
             });
 

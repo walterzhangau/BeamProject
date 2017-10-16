e9791bfa66a17f2970632024a37ac177946a8d13
Evan Lewis <evanl2@student.unimelb.edu.au>
2017-10-16 21:38:22 +1100

    Deleted sendmessage.java and socketconnection.java
    added comments

--- Beam/app/src/main/java/com/example/grace/UserInformation/UserCredentials.java
+++ Beam/app/src/main/java/com/example/grace/UserInformation/UserCredentials.java
@@ -4,6 +4,8 @@ package com.example.grace.UserInformation;
  * Created by evanl on 9/10/2017.
  */
 
+
+// User credentials of logged in user
 public class UserCredentials {
 
     public static String email;

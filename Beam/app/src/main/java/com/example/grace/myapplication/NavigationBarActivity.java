563243393ca85794ab2960d887a40a1247515c6d
Evan Lewis <evanl2@student.unimelb.edu.au>
2017-10-17 15:52:30 +1100

    Removed redundant parts of main menu

--- Beam/app/src/main/java/com/example/grace/myapplication/NavigationBarActivity.java
+++ Beam/app/src/main/java/com/example/grace/myapplication/NavigationBarActivity.java
@@ -42,27 +42,6 @@ public class NavigationBarActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_bar);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
 
         SetupActivityButtons();
         if (Build.VERSION.SDK_INT >= 23) {

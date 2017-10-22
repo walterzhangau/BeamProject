26c1cf897d658a1eeb1156cac983fba513a48c5f
tscelsi <tscelsi@student.unimelb.edu.au>
2017-10-22 21:39:06 +1100

    added comments

--- Beam/app/src/main/java/com/example/grace/ARchitect/AbstractArchitectCamActivity.java
+++ Beam/app/src/main/java/com/example/grace/ARchitect/AbstractArchitectCamActivity.java
@@ -59,46 +59,13 @@ public abstract class AbstractArchitectCamActivity extends Activity implements A
 	protected ArchitectView architectView;
 
 	/**
-	 * sensor accuracy listener in case you want to display calibration hints
-	 */
-	protected SensorAccuracyChangeListener sensorAccuracyListener;
-
-	/**
-	 * last known location of the user, used internally for content-loading after user location was fetched
-	 */
-	protected Location lastKnownLocaton;
-
-	/**
-	 * sample location strategy, you may implement a more sophisticated approach too
-	 */
-	protected ILocationProvider	locationProvider;
-
-	/**
-	 * location listener receives location updates and must forward them to the architectView
-	 */
-	protected LocationListener locationListener;
-
-	/**
-	 * JS interface listener handling e.g. 'AR.platform.sendJSONObject({foo:"bar", bar:123})' calls in JavaScript
-	 */
-	protected ArchitectJavaScriptInterfaceListener mArchitectJavaScriptInterfaceListener;
-
-	/**
 	 * worldLoadedListener receives calls when the AR world is finished loading or when it failed to laod.
 	 */
 	protected ArchitectView.ArchitectWorldLoadedListener worldLoadedListener;
 
-	protected JSONArray poiData;
-
-	protected boolean isLoading = false;
-
 	protected ArrayList<String> permissions = new ArrayList<String>();
 
 	protected String[] permissionsToCheck = new String[] {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
-
-	private String friendLocation;
-
-	private Location location = null;
 
 	protected String friendName;
 
@@ -141,13 +108,14 @@ public abstract class AbstractArchitectCamActivity extends Activity implements A
 		/* set AR-view for life-cycle notifications etc. */
 		this.architectView = (ArchitectView)this.findViewById( this.getArchitectViewId()  );
 
+		// finds out which user has been 'beamed' by extracting their username from the intent
 		Bundle extras = getIntent().getExtras();
 		friendName = extras.getString("user");
 
-		/* pass SDK key if you have one, this one is only valid for this package identifier and must not be used somewhere else */
+		/* set up the architectView passing in license key etc.
+		* */
 		final ArchitectStartupConfiguration config = new ArchitectStartupConfiguration();
 		config.setLicenseKey(this.getWikitudeSDKLicenseKey());
-		this.architectView.setCameraLifecycleListener(getCameraLifecycleListener());
 		try {
 			/* first mandatory life-cycle notification */
 			this.architectView.onCreate( config );
@@ -157,16 +125,13 @@ public abstract class AbstractArchitectCamActivity extends Activity implements A
 			Log.e(this.getClass().getName(), "Exception in ArchitectView.onCreate()", rex);
 		}
 
-		// set world loaded listener if implemented
+		// set world loaded listener
 		this.worldLoadedListener = this.getWorldLoadedListener();
 
-		// register valid world loaded listener in architectView, ensure this is set before content is loaded to not miss any event
+		// registers valid world loaded listener in architectView
 		if (this.worldLoadedListener != null && this.architectView != null) {
 			this.architectView.registerWorldLoadedListener(worldLoadedListener);
 		}
-
-		// set accuracy listener if implemented, you may e.g. show calibration prompt for compass using this listener
-		this.sensorAccuracyListener = this.getSensorAccuracyListener();
 
 		// create timer and a new timer task that gets the location of friend
 		// from server and calls setLocation() with the correct longitude and latitude
@@ -186,6 +151,8 @@ public abstract class AbstractArchitectCamActivity extends Activity implements A
 
 	}
 
+	/* sends request to server for friends location, response handled in ASyncTask FriendLocationTask
+	* */
 	private boolean getFriendLocation() {
 		Log.e(TAG, "getFriendLocation");
 		ServerConnection serverConnection = new ServerConnection();
@@ -203,31 +170,8 @@ public abstract class AbstractArchitectCamActivity extends Activity implements A
 		return true;
 	}
 
-
-	protected abstract CameraSettings.CameraPosition getCameraPosition();
-
-	private int getFeatures() {
-		int features = (hasGeo() ? ArchitectStartupConfiguration.Features.Geo : 0) |
-				(hasIR() ? ArchitectStartupConfiguration.Features.ImageTracking : 0) |
-				(hasInstant() ? ArchitectStartupConfiguration.Features.InstantTracking : 0) ;
-		return features;
-	}
-
-	protected abstract boolean hasGeo();
-	protected abstract boolean hasIR();
-	protected abstract boolean hasInstant();
-
-	protected CameraLifecycleListener getCameraLifecycleListener() {
-		return null;
-	}
-
-	protected CameraSettings.CameraResolution getCameraResolution(){
-		return CameraSettings.CameraResolution.AUTO;
-	}
-	protected boolean getCamera2Enabled() {
-		return false;
-	}
-
+	/* links the native ARWorld with the java in architectView.load
+	* */
 	@Override
 	protected void onPostCreate( final Bundle savedInstanceState ) {
 		super.onPostCreate( savedInstanceState );
@@ -258,18 +202,9 @@ public abstract class AbstractArchitectCamActivity extends Activity implements A
 		super.onResume();
 
 		// call mandatory live-cycle method of architectView
-		if ( this.architectView != null ) {
+		if (this.architectView != null) {
 			this.architectView.onResume();
 
-			// register accuracy listener in architectView, if set
-			if (this.sensorAccuracyListener!=null) {
-				this.architectView.registerSensorAccuracyChangeListener( this.sensorAccuracyListener );
-			}
-		}
-
-		// tell locationProvider to resume, usually location is then (again) fetched, so the GPS indicator appears in status bar
-		if ( this.locationProvider != null ) {
-			this.locationProvider.onResume();
 		}
 	}
 
@@ -280,16 +215,6 @@ public abstract class AbstractArchitectCamActivity extends Activity implements A
 		// call mandatory live-cycle method of architectView
 		if ( this.architectView != null ) {
 			this.architectView.onPause();
-
-			// unregister accuracy listener in architectView, if set
-			if ( this.sensorAccuracyListener != null ) {
-				this.architectView.unregisterSensorAccuracyChangeListener( this.sensorAccuracyListener );
-			}
-		}
-
-		// tell locationProvider to pause, usually location is then no longer fetched, so the GPS indicator disappears in status bar
-		if ( this.locationProvider != null ) {
-			this.locationProvider.onPause();
 		}
 	}
 
@@ -318,14 +243,7 @@ public abstract class AbstractArchitectCamActivity extends Activity implements A
 	}
 
 	/**
-	 * title shown in activity
-	 * @return
-	 */
-	public abstract String getActivityTitle();
-
-	/**
 	 * path to the architect-file (AR-Experience HTML) to launch
-	 * @return
 	 */
 	@Override
 	public abstract String getARchitectWorldPath();
@@ -355,143 +273,10 @@ public abstract class AbstractArchitectCamActivity extends Activity implements A
 	public abstract int getArchitectViewId();
 
 	/**
-	 *
-	 * @return Implementation of a Location
-	 */
-	@Override
-	public abstract ILocationProvider getLocationProvider(final LocationListener locationListener);
-
-	/**
-	 * @return Implementation of Sensor-Accuracy-Listener. That way you can e.g. show prompt to calibrate compass
-	 */
-	@Override
-	public abstract ArchitectView.SensorAccuracyChangeListener getSensorAccuracyListener();
-
-	/**
 	 * @return Implementation of ArchitectWorldLoadedListener. That way you know when a AR world is finished loading or when it failed to load.
 	 */
 	@Override
 	public abstract ArchitectView.ArchitectWorldLoadedListener getWorldLoadedListener();
-
-	/**
-	 * helper to check if video-drawables are supported by this device. recommended to check before launching ARchitect Worlds with videodrawables
-	 * @return true if AR.VideoDrawables are supported, false if fallback rendering would apply (= show video fullscreen)
-	 */
-	public static final boolean isVideoDrawablesSupported() {
-		String extensions = GLES20.glGetString( GLES20.GL_EXTENSIONS );
-		return extensions != null && extensions.contains( "GL_OES_EGL_image_external" );
-	}
-
-	protected void injectData() {
-		if (!isLoading) {
-			final Thread t = new Thread(new Runnable() {
-
-				@Override
-				public void run() {
-
-					isLoading = true;
-
-					final int WAIT_FOR_LOCATION_STEP_MS = 2000;
-
-					while (lastKnownLocaton==null && !isFinishing()) {
-
-						runOnUiThread(new Runnable() {
-
-							@Override
-							public void run() {
-								Toast.makeText(AbstractArchitectCamActivity.this, "fetching location", Toast.LENGTH_SHORT).show();
-							}
-						});
-
-						try {
-							Thread.sleep(WAIT_FOR_LOCATION_STEP_MS);
-						} catch (InterruptedException e) {
-							break;
-						}
-					}
-
-					if (lastKnownLocaton!=null && !isFinishing()) {
-						// TODO: you may replace this dummy implementation and instead load POI information e.g. from your database
-						poiData = getPoiInformation(lastKnownLocaton, 20);
-						callJavaScript("World.loadPoisFromJsonData", new String[] { poiData.toString() });
-					}
-
-					isLoading = false;
-				}
-			});
-			t.start();
-		}
-	}
-
-	/**
-	 * call JacaScript in architectView
-	 * @param methodName
-	 * @param arguments
-	 */
-	private void callJavaScript(final String methodName, final String[] arguments) {
-		final StringBuilder argumentsString = new StringBuilder("");
-		for (int i= 0; i<arguments.length; i++) {
-			argumentsString.append(arguments[i]);
-			if (i<arguments.length-1) {
-				argumentsString.append(", ");
-			}
-		}
-
-		if (this.architectView!=null) {
-			final String js = ( methodName + "( " + argumentsString.toString() + " );" );
-			this.architectView.callJavascript(js);
-		}
-	}
-
-	/**
-	 * loads poiInformation and returns them as JSONArray. Ensure attributeNames of JSON POIs are well known in JavaScript, so you can parse them easily
-	 * @param userLocation the location of the user
-	 * @param numberOfPlaces number of places to load (at max)
-	 * @return POI information in JSONArray
-	 */
-	public static JSONArray getPoiInformation(final Location userLocation, final int numberOfPlaces) {
-
-		if (userLocation==null) {
-			return null;
-		}
-
-		final JSONArray pois = new JSONArray();
-
-		// ensure these attributes are also used in JavaScript when extracting POI data
-		final String ATTR_ID = "id";
-		final String ATTR_NAME = "name";
-		final String ATTR_DESCRIPTION = "description";
-		final String ATTR_LATITUDE = "latitude";
-		final String ATTR_LONGITUDE = "longitude";
-		final String ATTR_ALTITUDE = "altitude";
-
-		for (int i=1;i <= numberOfPlaces; i++) {
-			final HashMap<String, String> poiInformation = new HashMap<String, String>();
-			poiInformation.put(ATTR_ID, String.valueOf(i));
-			poiInformation.put(ATTR_NAME, "POI#" + i);
-			poiInformation.put(ATTR_DESCRIPTION, "This is the description of POI#" + i);
-			double[] poiLocationLatLon = getRandomLatLonNearby(userLocation.getLatitude(), userLocation.getLongitude());
-			poiInformation.put(ATTR_LATITUDE, String.valueOf(poiLocationLatLon[0]));
-			poiInformation.put(ATTR_LONGITUDE, String.valueOf(poiLocationLatLon[1]));
-			final float UNKNOWN_ALTITUDE = -32768f;  // equals "AR.CONST.UNKNOWN_ALTITUDE" in JavaScript (compare AR.GeoLocation specification)
-			// Use "AR.CONST.UNKNOWN_ALTITUDE" to tell ARchitect that altitude of places should be on user level. Be aware to handle altitude properly in locationManager in case you use valid POI altitude value (e.g. pass altitude only if GPS accuracy is <7m).
-			poiInformation.put(ATTR_ALTITUDE, String.valueOf(UNKNOWN_ALTITUDE));
-			pois.put(new JSONObject(poiInformation));
-		}
-
-		return pois;
-	}
-
-	/**
-	 * helper for creation of dummy places.
-	 * @param lat center latitude
-	 * @param lon center longitude
-	 * @return lat/lon values in given position's vicinity
-	 */
-	private static double[] getRandomLatLonNearby(final double lat, final double lon) {
-		return new double[] { lat + Math.random()/5-0.1 , lon + Math.random()/5-0.1};
-	}
-
 
 	public boolean checkPermission(String permission) {
 		if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
@@ -517,9 +302,6 @@ public abstract class AbstractArchitectCamActivity extends Activity implements A
 
 	public class FriendLocationTask extends AsyncTask<Void, Void, Boolean> {
 
-
-
-
 		@Override
 		protected Boolean doInBackground(Void... params) {
 			Log.e(TAG, "doInBackground");
@@ -537,7 +319,6 @@ public abstract class AbstractArchitectCamActivity extends Activity implements A
 
 			return false;
 		}
-
 
 		@Override
 		protected void onPostExecute(final Boolean success) {
@@ -567,7 +348,5 @@ public abstract class AbstractArchitectCamActivity extends Activity implements A
 
 		}
 	}
-
-
 
 }
